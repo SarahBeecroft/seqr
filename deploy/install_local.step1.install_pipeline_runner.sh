ca2124e7ca912497a277d66de53b78a4cf2ec565
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-02-26 17:09:15 +0800

    Update install_local.step1.install_pipeline_runner.sh
    update method for adding spark home to ~/.bashrc

--- deploy/install_local.step1.install_pipeline_runner.sh
+++ deploy/install_local.step1.install_pipeline_runner.sh
@@ -16,7 +16,7 @@ cd ${SEQR_BIN_DIR} \
     && wget -nv https://archive.apache.org/dist/spark/spark-2.0.2/${SPARK_VERSION}.tgz && tar xzf ${SPARK_VERSION}.tgz && rm ${SPARK_VERSION}.tgz
 
 export SPARK_HOME=${SEQR_BIN_DIR}'/'${SPARK_VERSION}
-sudo echo 'export SPARK_HOME='${SPARK_HOME} >> ~/.bashrc
+cat <(echo 'export SPARK_HOME='${SPARK_HOME}) ~/.bashrc > /tmp/bashrc && mv /tmp/bashrc ~/.bashrc
 
 set +x
 echo
