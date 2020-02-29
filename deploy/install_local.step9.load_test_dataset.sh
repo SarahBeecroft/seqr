6980f3bab403454c66c024e85417ddef1f07e2a1
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-02-29 22:07:24 +0800

    Update install_local.step9.load_test_dataset.sh

--- deploy/install_local.step9.load_test_dataset.sh
+++ deploy/install_local.step9.load_test_dataset.sh
@@ -19,7 +19,7 @@ cd ${SEQR_DIR}/hail_elasticsearch_pipelines
 
 PIPELINE_CPU_LIMIT=2
 
-nohup python2.7 gcloud_dataproc/submit.py --cpu-limit $PIPELINE_CPU_LIMIT --num-executors $PIPELINE_CPU_LIMIT  --run-locally hail_scripts/v01/load_dataset_to_es.py \
+nohup python3 gcloud_dataproc/submit.py --cpu-limit $PIPELINE_CPU_LIMIT --num-executors $PIPELINE_CPU_LIMIT  --run-locally hail_scripts/v01/load_dataset_to_es.py \
     --spark-home $SPARK_HOME --genome-version 37 --project-guid R001_test --sample-type WES --dataset-type VARIANTS \
     --skip-validation  --exclude-hgmd --vep-block-size 10 --es-block-size 10 --num-shards 1 --hail-version 0.1 \
     --use-nested-objects-for-vep --use-nested-objects-for-genotypes \
