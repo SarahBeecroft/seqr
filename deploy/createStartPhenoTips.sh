b4fc504731da5f5935e21c8516b05bed8f468bbd
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-02-29 23:10:13 +0800

    Update createStartPhenoTips.sh

--- deploy/createStartPhenoTips.sh
+++ deploy/createStartPhenoTips.sh
@@ -7,4 +7,7 @@ LOG_FILE=$(pwd)/phenotips.log
 (nohup ./start.sh >& ${LOG_FILE}) &
 echo "PhenoTips started in background on port 8080. See ${LOG_FILE}"
 ' | tee start_phenotips.sh
-chmod 777 ./start_phenotips.sh
+
+chmod 777 start_phenotips.sh
+
+./start_phenotips.sh
