69c82755ec5a7b52d30f8093fd103757c4dd6a14
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2020-02-29 20:40:02 +0800

    Update configElasticsearch.sh
    added steps Install gcloud sdk, Clone the seqr repo, init gsutil, init utilities

--- deploy/configElasticsearch.sh
+++ deploy/configElasticsearch.sh
@@ -43,3 +43,92 @@ fi
 
 # apply limit to current session
 sudo prlimit --pid $$ --nofile=65536
+
+
+set +x
+echo ==== Install gcloud sdk =====
+set -x
+
+# copied from https://cloud.google.com/sdk/docs/quickstart-debian-ubuntu
+
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk
+
+# make sure crcmod is installed for copying files with gsutil
+sudo apt-get install -y gcc python-dev python-setuptools
+sudo easy_install -U pip
+sudo pip uninstall crcmod
+sudo pip install -U crcmod
+
+set +x
+
+echo "==== Clone the seqr repo ====="
+set -x
+
+export SEQR_BRANCH=master
+
+git clone --recursive https://github.com/SarahBeecroft/seqr.git
+cd seqr/
+git checkout $SEQR_BRANCH
+cd ..
+
+set +x
+
+echo "===== init gsutil ====="
+
+# Add a generic key for accessing public google cloud storage buckets
+# Using a top-level /.config directory so that config files (like core-site.xml) can be shared with the Docker container
+sudo mkdir -p /.config/
+sudo chmod 777 /.config/
+cp ${SEQR_DIR}/deploy/secrets/shared/gcloud/* /.config/
+
+if [ -e "/.config/service-account-key.json" ]; then
+    # authenticate to google cloud using service account
+    gcloud auth activate-service-account --key-file /.config/service-account-key.json
+    cp /.config/boto ~/.boto
+    sudo mv /etc/boto.cfg /etc/boto.cfg.aside  # /etc/boto.cfg leads to "ImportError: No module named google_compute_engine" on gcloud Ubuntu VMs, so move it out of the way
+fi
+
+
+# check that gsutil works and is able to access gs://hail-common/
+GSUTIL_TEST="gsutil ls gs://hail-common/vep"
+$GSUTIL_TEST
+if [ $? -eq 0 ]; then
+    echo gsutil works
+else
+    echo "$GSUTIL_TEST failed - unable to access public gs://hail-common bucket."
+    echo "Try running 'gcloud init'. "
+    exit 1
+fi
+
+echo "===== init utilities ====="
+# install tabix, bgzip, samtools - which may be needed for VEP and the loading pipeline
+mkdir -p $SEQR_BIN_DIR
+gsutil -m cp gs://hail-common/vep/htslib/* ${SEQR_BIN_DIR}/ \
+    && gsutil -m cp gs://hail-common/vep/samtools ${SEQR_BIN_DIR}/ \
+    && chmod a+rx  ${SEQR_BIN_DIR}/tabix ${SEQR_BIN_DIR}/bgzip ${SEQR_BIN_DIR}/htsfile ${SEQR_BIN_DIR}/samtools
+
+
+if [ "$needs_reboot" ] ; then
+
+  echo '
+  ==================================================================
+  Config changes above will take effect after a reboot.
+  ==================================================================
+'
+    read -p "Reboot now? [y/n] " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Shutting down..."
+        sudo reboot
+    else
+        echo "Skipping reboot."
+    fi
+fi
